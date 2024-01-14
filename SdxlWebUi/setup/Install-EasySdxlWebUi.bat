d55d39bac0835b1e2bc4edd89f1fbb808a294266
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-01-14 13:58:27 +0900

    crlfでコミット。

---
+++ SdxlWebUi/setup/Install-EasySdxlWebUi.bat
@@ -0,0 +1,76 @@
+@echo off
+chcp 65001 > NUL
+pushd %~dp0
+set PS_CMD=PowerShell -Version 5.1 -ExecutionPolicy Bypass
+
+set CURL_CMD=C:\Windows\System32\curl.exe
+if not exist %CURL_CMD% (
+	echo [ERROR] %CURL_CMD% が見つかりません。
+	pause & popd & exit /b 1
+)
+
+echo 以下の配布元から関連ファイルをダウンロードして使用します（URL を Ctrl + クリックで開けます）。
+echo https://www.python.org
+echo https://github.com/pypa/get-pip
+echo https://github.com/git-for-windows
+echo https://github.com/AUTOMATIC1111/stable-diffusion-webui
+echo https://huggingface.co/cagliostrolab/animagine-xl-3.0
+echo https://huggingface.co/furusu/SD-LoRA
+echo https://huggingface.co/latent-consistency/lcm-lora-sdxl
+echo https://huggingface.co/2vXpSwA7/iroiro-lora
+echo https://huggingface.co/lllyasviel/sd_control_collection
+echo https://github.com/DominikDoom/a1111-sd-webui-tagcomplete
+echo https://github.com/Bing-su/adetailer
+echo https://github.com/pkuliyi2015/multidiffusion-upscaler-for-automatic1111
+echo https://github.com/adieyal/sd-dynamic-prompts
+echo https://github.com/continue-revolution/sd-webui-animatediff
+echo https://github.com/hako-mikan/sd-webui-cd-tuner
+echo https://github.com/Mikubill/sd-webui-controlnet
+echo https://github.com/hnmr293/sd-webui-cutoff
+echo https://github.com/hako-mikan/sd-webui-lora-block-weight
+echo https://github.com/hako-mikan/sd-webui-negpip
+echo https://github.com/zixaphir/Stable-Diffusion-Webui-Civitai-Helper
+echo https://github.com/Katsuyuki-Karasawa/stable-diffusion-webui-localization-ja_JP
+echo https://github.com/arenasys/stable-diffusion-webui-model-toolkit
+echo https://github.com/picobyte/stable-diffusion-webui-wd14-tagger
+echo https://github.com/hako-mikan/sd-webui-supermerger
+echo https://huggingface.co/spaces/Linaqruf/animagine-xl
+echo https://br-d.fanbox.cc/posts/5680274
+echo https://rentry.org/NAIwildcards
+echo よろしいですか？ [y/n]
+set /p YES_OR_NO=
+if /i not "%YES_OR_NO%" == "y" ( popd & exit /b 1 )
+
+set EASY_SDXL_WEBUI_DIR=EasySdxlWebUi-main
+
+if not exist SdxlWebUi\setup\lib\ ( mkdir SdxlWebUi\setup\lib )
+
+echo %CURL_CMD% -Lo SdxlWebUi\setup\lib\EasySdxlWebUi.zip https://github.com/Zuntan03/EasySdxlWebUi/archive/refs/heads/main.zip
+%CURL_CMD% -Lo SdxlWebUi\setup\lib\EasySdxlWebUi.zip https://github.com/Zuntan03/EasySdxlWebUi/archive/refs/heads/main.zip
+if %errorlevel% neq 0 ( pause & popd & exit /b %errorlevel% )
+
+echo %PS_CMD% Expand-Archive -Path SdxlWebUi\setup\lib\EasySdxlWebUi.zip -DestinationPath SdxlWebUi\setup\lib -Force
+%PS_CMD% Expand-Archive -Path SdxlWebUi\setup\lib\EasySdxlWebUi.zip -DestinationPath SdxlWebUi\setup\lib -Force
+if %errorlevel% neq 0 ( pause & popd & exit /b %errorlevel% )
+
+echo del SdxlWebUi\setup\lib\EasySdxlWebUi.zip
+del SdxlWebUi\setup\lib\EasySdxlWebUi.zip
+if %errorlevel% neq 0 ( pause & popd & exit /b %errorlevel% )
+
+echo /QSY .\SdxlWebUi\setup\lib\%EASY_SDXL_WEBUI_DIR%\ .
+xcopy /QSY .\SdxlWebUi\setup\lib\%EASY_SDXL_WEBUI_DIR%\ .
+
+echo copy /Y .\SdxlWebUi\setup\Install-EasySdxlWebUi.bat .\SdxlWebUi-Update.bat
+copy /Y .\SdxlWebUi\setup\Install-EasySdxlWebUi.bat .\SdxlWebUi-Update.bat
+
+call SdxlWebUi\setup\Setup-SdxlWebUi.bat
+if %errorlevel% neq 0 ( popd & exit /b %errorlevel% )
+
+call SdxlWebUi-ResetConfig.bat
+if %errorlevel% neq 0 ( popd & exit /b %errorlevel% )
+
+start "" SdxlWebUi.bat
+
+popd rem %~dp0
+
+if exist %~dp0Install-EasySdxlWebUi.bat ( del %~dp0Install-EasySdxlWebUi.bat )
---
+++ SdxlWebUi/setup/SetGitPath.bat
@@ -0,0 +1,35 @@
+@echo off
+chcp 65001 > NUL
+set CURL_CMD=C:\Windows\System32\curl.exe
+set PS_CMD=PowerShell -Version 5.1 -ExecutionPolicy Bypass
+
+where /Q git
+if %ERRORLEVEL% equ 0 ( exit /b 0 )
+cd > NUL
+set PORTABLE_GIT_DIR=%~dp0lib\PortableGit\bin
+
+if not exist %PORTABLE_GIT_DIR%\ (
+	if not exist %~dp0lib\ ( mkdir %~dp0lib )
+
+	echo %CURL_CMD% -Lo %~dp0lib\PortableGit.7z.exe https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/PortableGit-2.43.0-64-bit.7z.exe
+	%CURL_CMD% -Lo %~dp0lib\PortableGit.7z.exe https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/PortableGit-2.43.0-64-bit.7z.exe
+	if %errorlevel% neq 0 ( pause & exit /b %errorlevel% )
+
+	start "" %PS_CMD% -Command "Start-Sleep -Seconds 2; $title='Portable Git for Windows 64-bit'; $window=Get-Process | Where-Object { $_.MainWindowTitle -eq $title } | Select-Object -First 1; if ($window -ne $null) { [void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic'); [Microsoft.VisualBasic.Interaction]::AppActivate($window.Id); Start-Sleep -Seconds 1; Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('{ENTER}') }"
+
+	echo 設定を変更せずに、そのままインストールしてください。
+	%~dp0lib\PortableGit.7z.exe
+	if %errorlevel% neq 0 ( pause & exit /b %errorlevel% )
+
+	echo del %~dp0lib\PortableGit.7z.exe
+	del %~dp0lib\PortableGit.7z.exe
+	if %errorlevel% neq 0 ( pause & exit /b %errorlevel% )
+)
+
+set PATH=%PORTABLE_GIT_DIR%;%PATH%
+
+where /Q git
+if %ERRORLEVEL% equ 0 ( exit /b 0 )
+
+echo [Error] git をインストールできませんでした。手動で Git for Windows をインストールしてください。
+pause & exit /b 1
