8877d12f8a1d8175720eecfdda1ab7d130a818a1
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-02-07 00:17:59 +0900

    Zero123モデルのダウンロードbat

---
+++ Download/forge-zero123.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001 > NUL
+
+set CURL_CMD=C:\Windows\System32\curl.exe
+
+if not exist %CURL_CMD% (
+	echo [ERROR] %CURL_CMD% が見つかりません。
+	pause & exit /b 1
+)
+
+%CURL_CMD% -Lo %~dp0\..\SdxlWebUi\stable-diffusion-webui-forge\models\z123\stable_zero123.ckpt ^
+https://huggingface.co/stabilityai/stable-zero123/resolve/main/stable_zero123.ckpt
+if %errorlevel% neq 0 ( pause & exit /b %errorlevel% )
