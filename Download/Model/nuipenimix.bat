bbf7725159dd12cefc22844ac70ceb3a5d8ee1bd
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-02-17 12:47:28 +0900

    ダウンロードできるモデル `Download/Model/` の更新と追加をしました。

--- Download/Model/nuipenimix.bat
+++ Download/Model/nuipenimix.bat
@@ -1,13 +1,13 @@
 @echo off
 chcp 65001 > NUL
 
-start "" https://civitai.com/models/296650
+start "" https://civitai.com/models/81937
 
 set SDXL_WEB_UI=%~dp0..\..\SdxlWebUi
 pushd %SDXL_WEB_UI%\Model
 
-call %SDXL_WEB_UI%\setup\Download.bat . rumblexl_v12.safetensors ^
-https://civitai.com/api/download/models/343419
+call %SDXL_WEB_UI%\setup\Download.bat . nuipenimix_ver3.safetensors ^
+https://civitai.com/api/download/models/333698
 if %errorlevel% neq 0 ( popd & exit /b %errorlevel% )
 
 popd rem %SDXL_WEB_UI%\Model
