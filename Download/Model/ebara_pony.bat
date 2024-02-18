bb10f461267d2705a8647dc5b18694b4f527091a
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-02-18 12:39:28 +0900

    ebara_ponyのBakedVAE版が何故か読み込めないので無効化

--- Download/Model/ebara_pony.bat
+++ Download/Model/ebara_pony.bat
@@ -6,8 +6,16 @@ start "" https://huggingface.co/tsukihara/xl_model
 set SDXL_WEB_UI=%~dp0..\..\SdxlWebUi
 pushd %SDXL_WEB_UI%\Model
 
-call %SDXL_WEB_UI%\setup\Download.bat . ebara_pony_1.bakedVAE.safetensorss ^
-https://huggingface.co/tsukihara/xl_model/resolve/main/ebara_pony_1.bakedVAE.safetensors
+@REM call %SDXL_WEB_UI%\setup\Download.bat . ebara_pony_1.bakedVAE.safetensorss ^
+@REM https://huggingface.co/tsukihara/xl_model/resolve/main/ebara_pony_1.bakedVAE.safetensors
+@REM if %errorlevel% neq 0 ( popd & exit /b %errorlevel% )
+
+@REM call %SDXL_WEB_UI%\setup\Download.bat . ebara_pony_1.bakedVAE.png ^
+@REM https://huggingface.co/tsukihara/xl_model/resolve/main/ebara_pony_1.png
+@REM if %errorlevel% neq 0 ( popd & exit /b %errorlevel% )
+
+call %SDXL_WEB_UI%\setup\Download.bat . ebara_pony_1.safetensors ^
+https://huggingface.co/tsukihara/xl_model/resolve/main/ebara_pony_1.safetensors
 if %errorlevel% neq 0 ( popd & exit /b %errorlevel% )
 
 call %SDXL_WEB_UI%\setup\Download.bat . ebara_pony_1.png ^
