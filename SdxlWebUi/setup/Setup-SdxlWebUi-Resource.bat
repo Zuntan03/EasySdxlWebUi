764f91870a7e31e5eedd2742023791f3fe38dde0
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-02-25 16:01:12 +0900

    Lightning 2step LoRA をデフォルトリソースに追加。

--- SdxlWebUi/setup/Setup-SdxlWebUi-Resource.bat
+++ SdxlWebUi/setup/Setup-SdxlWebUi-Resource.bat
@@ -39,6 +39,11 @@ call %~dp0Download.bat Lora\iroiro sdxl-flat.safetensors ^
 https://huggingface.co/2vXpSwA7/iroiro-lora/resolve/main/sdxl/sdxl-flat.safetensors
 if %errorlevel% neq 0 ( popd & exit /b %errorlevel% )
 
+@REM SDXL-Lightning LoRA
+call %~dp0Download.bat Lora\SDXL-Lightning sdxl_lightning_2step_lora.safetensors ^
+https://huggingface.co/ByteDance/SDXL-Lightning/resolve/main/sdxl_lightning_2step_lora.safetensors
+if %errorlevel% neq 0 ( popd & exit /b %errorlevel% )
+
 @REM Upscaler
 call %~dp0Download.bat Upscaler 4x-UltraSharp.pth ^
 https://huggingface.co/Zuntan/dist/resolve/main/4x-UltraSharp.pth
