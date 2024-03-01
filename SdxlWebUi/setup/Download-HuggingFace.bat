1b7318f1ed416f5ef696bc5026f49bdcbf12dca3
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-03-02 05:09:30 +0900

    HuggingFaceからDLできない場合がある不具合の修正

--- SdxlWebUi/setup/Download-HuggingFace.bat
+++ SdxlWebUi/setup/Download-HuggingFace.bat
@@ -3,22 +3,13 @@ chcp 65001 > NUL
 
 if not exist %~dp0lib\ ( mkdir %~dp0lib )
 pushd %~dp0lib
-setlocal enabledelayedexpansion
-if exist venv\ (
-	call %~dp0ActivateVirtualEnvironment.bat
-	if !errorlevel! neq 0 ( popd & exit /b !errorlevel! )
 
-	@REM 一番最初のvenv対策
-	pip install -q huggingface_hub
-	if !errorlevel! neq 0 ( pause & popd & exit /b !errorlevel! )
-) else (
-	call %~dp0ActivateVirtualEnvironment.bat
-	if !errorlevel! neq 0 ( popd & exit /b !errorlevel! )
+call %~dp0ActivateVirtualEnvironment.bat
+if %errorlevel% neq 0 ( popd & exit /b %errorlevel% )
 
-	pip install -q huggingface_hub
-	if !errorlevel! neq 0 ( pause & popd & exit /b !errorlevel! )
-)
-endlocal
+pip install -q huggingface_hub
+if %errorlevel% neq 0 ( pause & popd & exit /b %errorlevel% )
+
 popd rem %~dp0lib
 
 python %~dp0download_huggingface.py %*
