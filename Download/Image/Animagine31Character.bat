f4bc3c4038099d5fb47f71d698745ed72d85c5d2
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-03-23 12:18:10 +0900

    キャラ画像のファイル名誤りの修正。 バッチサイズUI MAXを8>16に

--- Download/Image/Animagine31Character.bat
+++ Download/Image/Animagine31Character.bat
@@ -7,11 +7,12 @@ set DST_DIR=%~dp0..\..\save
 set BASE_NAME=Animagine31Character
 set ZIP_FILE_NAME=%BASE_NAME%.zip
 
-if exist "%DST_DIR%\%BASE_NAME%\0000-789-souryuu asuka langley, neon genesis evangelion.webp" ( exit /b 0 )
+if exist "%DST_DIR%\%BASE_NAME%\0000-789-1girl, souryuu asuka langley, neon genesis evangelion.webp" ( exit /b 0 )
 
-if not exist %DST_DIR% md %DST_DIR%
+if not exist %DST_DIR% ( md %DST_DIR% )
 pushd %DST_DIR%
 
+if exist %BASE_NAME%\ ( rmdir /S /Q %BASE_NAME%\ )
 if exist %ZIP_FILE_NAME% ( del %ZIP_FILE_NAME% )
 
 echo call %SDXL_WEB_UI%\setup\Download.bat . %ZIP_FILE_NAME% https://yyy.wpx.jp/EasySdxlWebUi/%ZIP_FILE_NAME%
