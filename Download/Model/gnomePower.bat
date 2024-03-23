5e5e2a56df18ede5593bb6e5adfa8101339df059
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-03-23 11:02:45 +0900

    モデルのバージョン更新

--- Download/Model/gnomePower.bat
+++ Download/Model/gnomePower.bat
@@ -6,8 +6,8 @@ start "" https://civitai.com/models/289041
 set SDXL_WEB_UI=%~dp0..\..\SdxlWebUi
 pushd %SDXL_WEB_UI%\Model
 
-call %SDXL_WEB_UI%\setup\Download.bat Pony gnomePower_v101.safetensors ^
-https://civitai.com/api/download/models/329384
+call %SDXL_WEB_UI%\setup\Download.bat Pony gnomePower_v111.safetensors ^
+https://civitai.com/api/download/models/367352
 if %errorlevel% neq 0 ( popd & exit /b %errorlevel% )
 
 popd rem %SDXL_WEB_UI%\Model
--- Download/Model/rumblexl.bat
+++ Download/Model/rumblexl.bat
@@ -6,8 +6,8 @@ start "" https://civitai.com/models/296650
 set SDXL_WEB_UI=%~dp0..\..\SdxlWebUi
 pushd %SDXL_WEB_UI%\Model
 
-call %SDXL_WEB_UI%\setup\Download.bat Anmg rumblexl_v12.safetensors ^
-https://civitai.com/api/download/models/343419
+call %SDXL_WEB_UI%\setup\Download.bat Anmg rumblexlAnimagineBased_v13.safetensors ^
+https://civitai.com/api/download/models/400175
 if %errorlevel% neq 0 ( popd & exit /b %errorlevel% )
 
 popd rem %SDXL_WEB_UI%\Model
